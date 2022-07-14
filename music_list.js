33598ddd6122c772e4641dcd16237b961c59062e
Tmt89 <tuan.tm193175@sis.hust.edu.vn>
2022-07-14 14:35:49 +0700

    first

---
+++ music_list.js
@@ -0,0 +1,130 @@
+const allMusic = [
+    {
+        name: 'Thích em hơi nhiều',
+        author: 'Wren Evans',
+        img: './assets/image/song_img/thichemhoinhieu.jpg',
+        src: './music_list/likeyoutoomuch.mp3'
+    },
+    {
+        name: 'Ngày đầu tiên',
+        author: 'Đức Phúc',
+        img: './assets/image/song_img/ngaydautien.jpg',
+        src: './music_list/ngaydautien.mp3'
+    },
+    {
+        name: 'Vài câu nói',
+        author: 'Grey D',
+        img: './assets/image/song_img/vaicaunoi.jpg',
+        src: './music_list/vaicaunoi.mp3'
+    },
+    {
+        name: 'Váy hoa nhí',
+        author: 'Hoàng Minh Châu',
+        img: './assets/image/song_img/vayhoanhi.jpg',
+        src: './music_list/vayhoanhi.mp3'
+    },
+    {
+        name: 'Ánh sao và bầu trời',
+        author: 'Tri',
+        img: './assets/image/song_img/AnhSao.jpg',
+        src: './music_list/ánhaovabautroi.mp3'
+    },
+    {
+        name: 'Tình đắng như ly cafe',
+        author: 'Nân, Ngơ',
+        img: './assets/image/song_img/tinhdangnhulycafe.jpg',
+        src: './music_list/tinhdangnhucafe.mp3'
+    },
+    {
+        name: 'Thói quen',
+        author: 'Hoàng Dũng',
+        img: './assets/image/song_img/thoiquen.jpg',
+        src: './music_list/thoiquen.mp3'
+    },
+    {
+        name: 'Phải giữ em thế nào',
+        author: 'Mr siro',
+        img: './assets/image/song_img/phaigiuemthenao.jpg',
+        src: './music_list/phaigiuemthenao.mp3'
+    },
+    {
+        name: 'Người khác',
+        author: 'Phan Mạnh Quỳnh',
+        img: './assets/image/song_img/nguoikhac.jpg',
+        src: './music_list/nguoikhac.mp3'
+    },
+    {
+        name: 'Một bước yêu vạn dặm đau',
+        author: 'Mr siro',
+        img: './assets/image/song_img/motbuocyeu.jpg',
+        src: './music_list/motbuocyeu.mp3'
+    },
+    {
+        name: 'In the Dark',
+        author: 'TIA x fueled by boba',
+        img: './assets/image/song_img/inthedark.jpg',
+        src: './music_list/doahoaluitan.mp3'
+    },
+    {
+        name: 'Day dứt nỗi đau',
+        author: 'Mr siro',
+        img: './assets/image/song_img/đayutnoidau.jpg',
+        src: './music_list/daydutnoidau.mp3'
+    },
+    {
+        name: 'Chuyện đôi ta',
+        author: 'Da lab',
+        img: './assets/image/song_img/chuyendoita.jpg',
+        src: './music_list/chuyendoita.mp3'
+    },
+    {
+        name: 'Chúng ta của sau này',
+        author: 'Tri',
+        img: './assets/image/song_img/chungtacúaunay.jpg',
+        src: './music_list/chungtacuasaunay.mp3'
+    },
+    {
+        name: 'Chờ lời anh nói',
+        author: 'Ivy, Crou',
+        img: './assets/image/song_img/choloianhnoi.jpg',
+        src: './music_list/choloianhnoi.mp3'
+    },
+    {
+        name: 'Chiều hôm ấy',
+        author: 'Jaiky',
+        img: './assets/image/song_img/chieuhomay.jpg',
+        src: './music_list/chieuhomay.mp3'
+    },
+    {
+        name: 'Chạm đáy nỗi đau',
+        author: 'Mr siro cover',
+        img: './assets/image/song_img/chamdaynoidau.jpg',
+        src: './music_list/chamdaynoidau.mp3'
+    },
+    {
+        name: 'Bước qua nhau',
+        author: 'Vũ',
+        img: './assets/image/song_img/bcquanhau.jpg',
+        src: './music_list/buocquanhau.mp3'
+    },
+    {
+        name: 'Bước qua mùa cô đơn',
+        author: 'Vũ',
+        img: './assets/image/song_img/bcquamuacodon.jpg',
+        src: './music_list/buocquamuacodon.mp3'
+    },
+    {
+        name: 'Bông hoa không thuộc về ta',
+        author: 'Việt',
+        img: './assets/image/song_img/Bonghoa.jpg',
+        src: './music_list/bonghoathuocveta.mp3'
+    },
+    {
+        name: 'Love me like that',
+        author: 'Nevertheless ost',
+        img: './assets/image/song_img/lovemelikethat.jpg',
+        src: './music_list/lovemelikethat.mp3'
+    },
+    
+]
+
